387b9b422a30abc69abf2776b9f7f614ba745d13
Vladimir Angirov <angirov@posteo.net>
2023-12-06 21:22:39 +0100

    Update truenas.sh

--- proxmox/truenas/truenas.sh
+++ proxmox/truenas/truenas.sh
@@ -1,2 +1,2 @@
-qm set 100 -scsi1 /dev/disk/by-id/ata-Samsung_SSD_870_QVO_4TB_S5STNF0W806397V
-qm set 100 -scsi2 /dev/disk/by-id/ata-Samsung_SSD_870_QVO_4TB_S5STNF0W712257L
+qm set 100 -scsi1 /dev/disk/by-id/ata-Samsung_SSD_870_QVO_4TB_S5STNF0W806397V,serial=S5STNF0W806397V
+qm set 100 -scsi2 /dev/disk/by-id/ata-Samsung_SSD_870_QVO_4TB_S5STNF0W712257L,serial=S5STNF0W712257L
